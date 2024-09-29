dea23fddda26d8f53e8cce8f82437d0c3596f1af
Estefani <tefydmarroquin@gmail.com>
2024-09-29 13:30:50 -0600

    Delete javascript.js
    Se le cambio nombre de javascript.js a Faq.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const accordionItemHeaders =document.querySelectorAll (".accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader  => {
-    accordionItemHeader.addEventListener("click",  event => {
-        accordionItemHeader.classList.toggle("active");
-    });
-});
